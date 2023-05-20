7deda5ac270ed3d0108e615456c78b0279f063dd
kazukazu123123 <50506519+kazukazu123123@users.noreply.github.com>
2023-05-20 15:50:01 +0900

    Revert "Change evt log to stderr"
    This reverts commit f0c38f18ee2d0193742d07b9057f6374c979fb30.

--- src/main.rs
+++ src/main.rs
@@ -25,11 +25,11 @@ struct Args {
 fn main() {
     let args = Args::parse();
 
-    eprintln!("evt_parsing");
+    println!("evt_parsing");
 
     let midi = MIDIFile::open(args.midi_file, None).unwrap();
 
-    eprintln!("evt_parsed");
+    println!("evt_parsed");
 
     let stats = pipe!(
       midi.iter_all_tracks()  
@@ -38,7 +38,7 @@ fn main() {
       |>get_channel_statistics().unwrap()
     );
 
-    eprintln!("evt_note_count,{}", stats.note_count());
+    println!("evt_note_count,{}", stats.note_count());
 
     let ppq = midi.ppq();
     let merged = pipe!(
@@ -54,7 +54,7 @@ fn main() {
     let now = Instant::now();
     let mut time = 0.0;
 
-    eprintln!("evt_playing");
+    println!("evt_playing");
 
     for e in merged {
         if e.delta != 0.0 {
@@ -71,5 +71,5 @@ fn main() {
             io::stdout().write_all(b"\n").unwrap();
         }
     }
-    eprintln!("evt_playing_finished");
+    println!("evt_playing_finished");
 }
